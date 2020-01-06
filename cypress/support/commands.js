ab66768c979f54e2f85b51b524454fd80844853d
javenkn <javenkn7@gmail.com>
2020-01-05 19:57:19 -1000

    Combines cypress custom commands to login as a new user

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,6 +9,23 @@ Cypress.Commands.add('createUser', overrides => {
   }).then(response => ({...response.body.user, ...user}))
 })
 
+Cypress.Commands.add('login', user => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3000/login',
+    body: user,
+  }).then(response => {
+    window.localStorage.setItem('token', response.body.user.token)
+    return {...response.body.user, ...user}
+  })
+})
+
+Cypress.Commands.add('loginAsNewUser', () => {
+  cy.createUser().then(user => {
+    cy.login(user)
+  })
+})
+
 Cypress.Commands.add('assertHome', () => {
   cy.url().should('eq', `${Cypress.config().baseUrl}/`)
 })
@@ -19,14 +36,4 @@ Cypress.Commands.add('assertLoggedInAs', user => {
     .should('be.a', 'string')
     .findByTestId('username-display')
     .should('have.text', user.username)
-})
-
-Cypress.Commands.add('login', user => {
-  cy.request({
-    method: 'POST',
-    url: 'http://localhost:3000/login',
-    body: user,
-  }).then(response => {
-    window.localStorage.setItem('token', response.body.user.token)
-  })
 })
