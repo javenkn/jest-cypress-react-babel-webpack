41f1a96f6c1da295c62f81f948b0e0f7f7f2d514
javenkn <javenkn7@gmail.com>
2020-01-05 16:07:12 -1000

    Adds test for login flow

---
+++ cypress/e2e/login.js
@@ -0,0 +1,38 @@
+import {buildUser} from '../support/generate'
+
+describe('login', () => {
+  it('should login an existing user', () => {
+    const user = buildUser()
+
+    cy.visit('/')
+      .findByText(/register/i)
+      .click()
+      .findByLabelText(/username/i)
+      .type(user.username)
+      .findByLabelText(/password/i)
+      .type(user.password)
+      .findByText(/submit/i)
+      .click()
+      .findByText(/logout/i)
+      .click()
+
+      // after registering a new user login tests can start
+      .findByText(/login/i)
+      .click()
+      .findByLabelText(/username/i)
+      .type(user.username)
+      .findByLabelText(/password/i)
+      .type(user.password)
+      .findByText(/submit/i)
+      .click()
+
+      // verifying user has login
+      .url()
+      .should('eq', `${Cypress.config().baseUrl}/`)
+      .window()
+      .its('localStorage.token')
+      .should('be.a', 'string')
+      .findByTestId('username-display')
+      .should('have.text', user.username)
+  })
+})
